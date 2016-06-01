6c091b2dabc2163bb02db936e9e5eced907fb098
Alexandre LIPPMANN <alexandre.lippmann@gmail.com>
2016-06-01 10:20:29 +0200

    hotfix

--- src/Gosyl/CommonBundle/Entity/ParamUsersRepository.php
+++ src/Gosyl/CommonBundle/Entity/ParamUsersRepository.php
@@ -6,7 +6,7 @@ use Gosyl\CommonBundle\Service\DataTable;
 
 class ParamUsersRepository extends EntityRepository {
 	public function getAllForDataTable($aWhere = null, $aExtras = null) {
-		$oDataTable = new DataTable();
+		$oDataTable = new DataTable($this->getEntityManager);
 		
 		$aRequest = array(
 				0 => 'U.id',
